3976d5fe45241aad39e5109046208c567eea7c8a
NickLatkovich <n.latkovich@pixelplex.by>
2018-03-31 18:18:52 +0300

    ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
-	"parserOptions": {
-		"ecmaVersion": 6
-	},
-	"env": {
-		"browser": true,
-		"jest": true
-	},
+	"env": { "browser": true, "node": true },
+	"extends": "eslint:recommended",
+	"parserOptions": { "sourceType": "module", "ecmaVersion": 8 },
 	"rules": {
 		"arrow-parens": [
 			"error",
@@ -17,6 +13,7 @@ module.exports = {
 				"classes": "always"
 			}
 		],
+		"no-console": 0,
 		"class-methods-use-this": 0,
 		"no-param-reassign": 0,
 		"no-tabs": 0,
--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
-const PIXI = require('pixi.js');
-const ImgBack = 'images/back.png';
+(async () => {
+	const PIXI = require('pixi.js');
+	const ImgBack = 'images/back.png';
+	const axios = require('axios');
 
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-	type = "canvas";
-}
+	let type = "WebGL";
+	if (!PIXI.utils.isWebGLSupported()) {
+		console.warn("WebGL is not supported");
+		type = "canvas";
+	}
 
-let app = new PIXI.Application({
-	antialias: true,
-	resolution: 1
-});
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.view.style.left = "0";
-app.renderer.view.style.top = "0";
-app.renderer.autoResize = true;
-app.renderer.resize(window.innerWidth, window.innerHeight);
+	let shaderCode = (await axios.get('/shaders/simple.glsl')).data;
+	let simpleShader = new PIXI.Filter(null, shaderCode);
 
-PIXI.loader
-	.add(ImgBack)
-	.load(setup);
+	let app = new PIXI.Application({
+		antialias: true,
+		resolution: 1
+	});
+	app.renderer.view.style.position = "absolute";
+	app.renderer.view.style.display = "block";
+	app.renderer.view.style.left = "0";
+	app.renderer.view.style.top = "0";
+	app.renderer.autoResize = true;
+	app.renderer.resize(window.innerWidth, window.innerHeight);
+	app.stage.filters = [simpleShader];
 
-function setup() {
-	let back = new PIXI.Sprite(PIXI.loader.resources[ImgBack].texture);
-	back.width = window.innerWidth;
-	back.height = window.innerHeight;
-	app.stage.addChild(back);
-}
+	PIXI.loader
+		.add(ImgBack)
+		.load(setup);
 
-document.body.appendChild(app.view);
+	function setup() {
+		let back = new PIXI.Sprite(PIXI.loader.resources[ImgBack].texture);
+		back.width = window.innerWidth;
+		back.height = window.innerHeight;
+		app.stage.addChild(back);
+	}
+
+	document.body.appendChild(app.view);
+})();
