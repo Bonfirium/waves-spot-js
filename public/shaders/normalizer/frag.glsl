2d6eb2da675dbbb0a8734c4c2b867e30c5f27f1d
NickLatkovich <n.latkovich@pixelplex.by>
2018-04-01 07:05:25 +0300

    Fix light color and add sky color

--- public/shaders/normalizer/frag.glsl
+++ public/shaders/normalizer/frag.glsl
@@ -7,6 +7,10 @@ const float p = 0.2;
 const float xd = p * sqrt(3.0) / 2.0;
 const float yd = p / 2.0;
 const float offset_power = 0.01;
+const vec3 lightDirection = normalize(vec3(1.0, -1.0, 1.0));
+const float halfPI = 3.1415926535 / 2.0;
+const vec4 lightColor = vec4(0.62, 0.86, 1.0, 0.7);
+const vec4 skyColor = vec4(0.26, 0.7, 0.95, 0.7);
 
 void main(void) {
 	float x, y;
@@ -27,10 +31,15 @@ void main(void) {
 	vec3 normal = normalize(cross(a, b));
 
 	gl_FragColor = texture2D(uSamplerOriginal, vTextureCoord + normal.xy * offset_power);
-	if (normal.x > 0.0) {
-		float al = normal.x / 2.0;
+	float angle = acos(dot(normal, lightDirection));
+	if (angle < halfPI) {
+		float al = 1.0 - angle / halfPI;
 		vec3 nrgb = (1.0 - gl_FragColor.rgb) * al;
-		gl_FragColor = gl_FragColor + vec4(nrgb, 0.0);
+		gl_FragColor = gl_FragColor + vec4(nrgb * lightColor.rgb * lightColor.a, 0.0);
+	} else {
+		float al = angle / halfPI - 1.0;
+		vec3 nrgb = (1.0 - gl_FragColor.rgb) * al;
+		gl_FragColor = gl_FragColor + vec4(nrgb * skyColor.rgb * skyColor.a, 0.0);
 	}
 //	gl_FragColor = gl_FragColor + vec4((1.0 - gl_FragColor.rgb) * (normal.x * 2.0), 1.0);
 }
